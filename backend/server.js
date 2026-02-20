b502685ed3147e9ab141d899241136c8487d77da
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-20 16:48:59 +0500

    added api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-import express from 'express';
+import express from "express";
 const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.send("You got 5 notes");
+})
 
 app.listen(5001, ()=>{
     console.log("Server running on port 5001");

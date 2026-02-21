9bc9d610c1ca111a1d32b214587cf9cc6ef4e9d3
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-21 07:06:17 +0500

    nodemon setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ import express from "express";
 const app = express();
 
 app.get('/api/notes', (req, res) => {
-    res.send("You got 5 notes");
+    res.status(200).send("You got 5 notes");
+})
+
+app.post('/api/notes', (req, res) => {
+    res.status(201).send("Your note has been created successfully");
 })
 
 app.listen(5001, ()=>{

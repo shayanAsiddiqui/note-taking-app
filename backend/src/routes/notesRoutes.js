018cffc8c149932c1bb01fb4ebc9565ea008f52a
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-21 10:22:59 +0500

    database setup

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createNote, deleteNote, getAllNotes} from "../controllers/notesController.js";
+import {createNote, deleteNote, getAllNotes, updateNote} from "../controllers/notesController.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get("/", getAllNotes);
 router.post("/", createNote);
 router.delete("/:id", deleteNote);
 router.put("/:id", updateNote);
+
 
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
+import dotenv from 'dotenv';
+import {connectDB} from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
+
 app.use("api/notes",notesRoutes);
 
-
-
-app.listen(5001, ()=>{
-    console.log("Server running on port 5001");
+app.listen(PORT, ()=>{
+    console.log("Server running on port ",PORT);
 })
+
+

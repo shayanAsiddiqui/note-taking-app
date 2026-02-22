a0d93e12ebeae8901ac77a2fb3f7828cb9fca2d3
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-22 15:57:40 +0500

    homepage

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,19 @@ import dotenv from 'dotenv';
 import {connectDB} from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use(express.json());
 app.use(rateLimiter)
+
 app.use("/api/notes",notesRoutes);
 
 

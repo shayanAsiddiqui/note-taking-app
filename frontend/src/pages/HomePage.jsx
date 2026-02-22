c25a75183897954b5921c8a4855b4625018d1f12
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-22 15:57:20 +0500

    setup homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,43 @@
+import  {Navbar}  from "../components/Navbar"
+import  RateLimitedUI  from "../components/RateLimitedUI"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+import axios from "axios"
+
 const HomePage = () => {
+  const [israteLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+        try {
+          const res = await axios.get("http://localhost:5001/api/notes");
+          console.log(res.data);
+          setNotes(res.data);
+          setRateLimited(false);
+        } catch (error) {
+          console.error("Error fetching notes:", error);
+          if(error.response?.status === 429){
+            setRateLimited(true);
+          }else{
+            toast.error("Failed to fetch notes");
+        }
+    }
+    finally{
+        setLoading(false);
+    }};
+
+    fetchNotes();
+  },[]);
+
   return (
-    <div>Homepage</div>
+    <div className="min-h-screen">
+      <Navbar />
+
+      {israteLimited && <RateLimitedUI />}
+
+    </div>
   )
 }
 export default HomePage

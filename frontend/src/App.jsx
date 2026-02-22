fd33ebf6349800c6297850012544fdef1f299df3
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-22 14:03:10 +0500

    setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import { Routes, Route } from "react-router"
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
 import NoteDetailPage from "./pages/NoteDetailPage"
+import toast from "react-hot-toast"
 
 export default function App(){
   return (
-    <div>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-      <Route path="/note/:id" element={<NoteDetailPage />} />
-    </Routes>
+    <div data-theme="synthwave">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
     )
 }

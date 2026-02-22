411054ba8973c11cda50539953a8cda2187728dd
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-22 21:30:30 +0500

    deployment setup

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -23,16 +23,18 @@ export async function createNote (req, res){
         res.status(500).json({message : "Internal Server Error"});
     }
 }
-export async function deleteNote (req, res){
-    try{
-        const deletedNote = await Note.findByIdAndDelete(req.params.id)
-        if(!deletedNote){res.status(404).json({message : "Note not found"})};
-        res.status(200).json({message : "Note deleted successfully" },{deletedNote});
-    }catch(error){
-        console.error("deleteNote", error);
-        res.status(500).json({message : "Internal Server Error"});
-    }
+
+export async function deleteNote(req, res) {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) return res.status(404).json({ message: "Note not found" });
+    res.status(200).json({ message: "Note deleted successfully!" });
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
 export async function updateNote (req, res){
     try {
         const{title, content} = req.body;

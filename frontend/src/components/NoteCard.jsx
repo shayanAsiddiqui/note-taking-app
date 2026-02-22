4688d6f9b65496d8f770da6767da914cf40148c9
shayanAsiddiqui <shayan.siddiqui.official@gmail.com>
2026-02-22 17:38:39 +0500

    delete funcitonality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,28 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";  
 import { formatDate } from "../lib/util";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note,setNotes }) => {
+
+    const handleDelete = async (e,id) => {
+        e.preventDefault();
+        
+        if(!window.confirm("Are you sure you want to delete this note?")){
+            return;
+        }
+
+        try {
+            await api.delete(`/notes/${id}`);
+            setNotes(prev => prev.filter(note => note._id !== id));
+            toast.success("Note deleted successfully");
+        } catch (error) {
+            console.log("Error deleting note", error);
+            toast.error("Failed to delete note");
+            
+        }
+    }
     return (
         <Link to={`/note/${note._id}`}
          className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]">
@@ -15,7 +35,7 @@ const NoteCard = ({ note }) => {
                     </span>
                     <div className="flex items-center gap-1">
                         <PenSquareIcon className = "size-4" />
-                        <button className="btn btn-ghost btn-xs text-error">
+                        <button className="btn btn-ghost btn-xs text-error" onClick={(e)=> handleDelete(e,note._id)}> 
                             <Trash2Icon className = "size-4" />
                         </button>
                     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import toast from "react-hot-toast"
 import api from "../lib/axios"
 import NoteCard from "../components/NoteCard"
-import axios from "axios"
+import NotesNotFound  from "../components/NotesNotFound"
 
 const HomePage = () => {
   const [israteLimited, setRateLimited] = useState(false);
@@ -42,10 +42,12 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
+        {notes.length === 0 && !israteLimited && !loading && <NotesNotFound />}
+
         {notes.length > 0 && !israteLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key = {note._id} note={note} />
+              <NoteCard key = {note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
           )}
